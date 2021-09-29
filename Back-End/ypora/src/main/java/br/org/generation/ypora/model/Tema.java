dd4361a2c7fca9581822ee9487da63a2118efd9b
unknown <vini98.2012@gmail.com>
2021-09-29 10:31:34 -0300

    adicionando anotações em Tema

--- Back-End/ypora/src/main/java/br/org/generation/ypora/model/Tema.java
+++ Back-End/ypora/src/main/java/br/org/generation/ypora/model/Tema.java
@@ -1,12 +1,33 @@
 package br.org.generation.ypora.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
+@Entity
+@Table(name = "tb_tema")
 public class Tema {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
+	
+	@NotNull(message = "O atributo nome é obrigatório")
+	@Size(min = 5, max = 100)
 	private String nome;
+	
+	@NotNull
+	@Size(min = 5, max = 500)
 	private String descricao;
+	
+	@NotNull
+	@Size(min = 5, max = 100)
 	private String tag;
+	
 	public long getId() {
 		return id;
 	}
