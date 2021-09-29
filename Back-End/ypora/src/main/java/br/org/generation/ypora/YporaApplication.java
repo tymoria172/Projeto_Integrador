2c9991cfac6cc8b89e35bc703e63e3eda01f3185
Proevento <andre.ricardo2115@gmail.com>
2021-09-29 09:59:59 -0300

    Criando Projeto Spring

---
+++ Back-End/ypora/src/main/java/br/org/generation/ypora/YporaApplication.java
@@ -0,0 +1,13 @@
+package br.org.generation.ypora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YporaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YporaApplication.class, args);
+	}
+
+}
