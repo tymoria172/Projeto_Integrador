5f4179f0a394581b5a4a6dc574a189210b157321
unknown <vini98.2012@gmail.com>
2021-10-29 10:50:40 -0300

    Iniciando o ambiente React

---
+++ Front-End/src/App.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Home from "./pages/home/Home";
+import Navbar from "./components/statics/navbar/Navbar";
+import Footer from "./components/statics/footer/Footer";
+import Login from "./pages/login/Login";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import Contato from "./pages/contato/Contato";
+import Sobre from "./pages/sobre/Sobre";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+
+      <Switch>
+        <div>
+          <Route exact path="/">
+            <Login />
+          </Route>
+
+          <Route path="/login">
+            <Login />
+          </Route>
+
+          <Route path="/home">
+            <Home />
+          </Route>
+
+          <Route path="/contato">
+            <Contato />
+          </Route>
+
+          <Route path="/sobre">
+            <Sobre />
+          </Route>
+        </div>
+      </Switch>
+
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
---
+++ Front-End/src/pages/contato/Contato.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Contato.css';
+
+function Contato() {
+    return (
+        <div>
+            <h1>Contato</h1>
+        </div>
+    )
+}
+
+export default Contato
