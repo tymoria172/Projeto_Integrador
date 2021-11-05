c237006829fb5fe29ff4aedda193762b54b6c1fc
Proevento <andre.ricardo2115@gmail.com>
2021-11-05 17:13:31 -0300

    arrumando home

--- Front-End/src/pages/home/Home.tsx
+++ Front-End/src/pages/home/Home.tsx
@@ -1,42 +1,151 @@
 import React from "react";
 import "./Home.css";
-import { Box, Grid,  } from "@material-ui/core";
+import { Box, Container, Paper, Button, Typography, Grid } from "@material-ui/core";
 
 function Home() {
-  return (
-    <>
-            <div style={{ width: '100%' }}>
-                <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gap: 1}}>
-
-                    <Box className="coluna1">
-                        <h1>ola</h1>
+    return (
+        <> 
+        <Box className="cor">
+        <Grid container className="container">
+            
+                <Box display="flex" justifyContent="center">
+                    <Box marginRight={1}>
                     </Box>
+                </Box>
+           
+            <Grid item xs={12}>
+               
+                <Box className="image">
 
+                   
+                </Box>
+            </Grid>
+        </Grid>
 
-                    <Grid container className="coluna2">
-                        <Grid item container direction='column' xs={12} sm={4}>
+        <Grid container className="container">
+            <Grid item xs={6}>
+                <Box paddingX={2} >
+                    <Typography 
+                    className="font"
+                    variant="h5" 
+                    component="h5" >
+    
+                    Somos oque somos para um futuro melhor 
+                    
+                    </Typography>
+                        <Typography variant="h6"  component="h6" align="center" className='font'>Pokémon elétricos são um dos tipos mais fortes em Pokémon Go porque eles têm apenas uma fraqueza real (Pokémon terrestre). Seus ataques são potencializados pelo tempo chuvoso, então espere ver muitos outros treinadores usando-os se o tempo for previsível. </Typography>
+    
+                  
 
-
-
-                            <Box className="filha"></Box>
-
-                        </Grid>
-
-                        <Box className="filha"></Box>
-                        <Box className="filha"></Box>
-
-
-                    </Grid>
-
-
-                    <Box className="coluna3">
-                        <h1>hello</h1>
+                </Box>
+                <Box display="flex" justifyContent="center">
+                    <Box marginRight={1}>
                     </Box>
 
                 </Box>
-            </div>
+            </Grid>
+
+            <Grid item xs={6} >
+                <Box p={1}>
+                    <img src="https://res.cloudinary.com/generation/image/upload/w_900,h_700,c_fill,q_auto:eco/brazil/2019/08/190603-GENERATION-469-1.jpg" alt="Eletric Pokemon" min-width='750px' height="450px" />
+                </Box>
+            </Grid>
+        </Grid>
+
+        <Grid container className="container">
+            <Grid item xs={6}>
+                <Box paddingX={2} >
+                    <Typography 
+                    className="font"
+                    variant="h5" 
+                    component="h5" >
+    
+                    Somos oque somos para um futuro melhor 
+                    
+                    </Typography>
+                        <Typography variant="h6"  component="h6" align="center" className='font'>Pokémon elétricos são um dos tipos mais fortes em Pokémon Go porque eles têm apenas uma fraqueza real (Pokémon terrestre). Seus ataques são potencializados pelo tempo chuvoso, então espere ver muitos outros treinadores usando-os se o tempo for previsível. </Typography>
+    
+                  
+
+                </Box>
+                <Box display="flex" justifyContent="center">
+                    <Box marginRight={1}>
+                    </Box>
+
+                </Box>
+            </Grid>
+
+            <Grid item xs={6} >
+                <Box p={1}>
+                    <img src="https://res.cloudinary.com/generation/image/upload/w_900,h_700,c_fill,q_auto:eco/brazil/2019/08/190603-GENERATION-469-1.jpg" alt="Eletric Pokemon" min-width='750px' height="450px" />
+                </Box>
+            </Grid>
+        </Grid>
+
+        <Grid container className="container">
+            <Grid item xs={6}>
+                <Box paddingX={2} >
+                    <Typography 
+                    className="font"
+                    variant="h5" 
+                    component="h5" >
+    
+                    Somos oque somos para um futuro melhor 
+                    
+                    </Typography>
+                        <Typography variant="h6"  component="h6" align="center" className='font'>Pokémon elétricos são um dos tipos mais fortes em Pokémon Go porque eles têm apenas uma fraqueza real (Pokémon terrestre). Seus ataques são potencializados pelo tempo chuvoso, então espere ver muitos outros treinadores usando-os se o tempo for previsível. </Typography>
+    
+                  
+
+                </Box>
+                <Box display="flex" justifyContent="center">
+                    <Box marginRight={1}>
+                    </Box>
+
+                </Box>
+            </Grid>
+
+            <Grid item xs={4} >
+                <Box p={1}>
+                    <img src="https://res.cloudinary.com/generation/image/upload/w_900,h_700,c_fill,q_auto:eco/brazil/2019/08/190603-GENERATION-469-1.jpg" alt="Eletric Pokemon" min-width='750px' height="450px" />
+                </Box>
+            </Grid>
+        </Grid>
+
+        <Grid container className="container">
+            <Grid item xs={12}>
+                <Box paddingX={2} >
+                    <Typography 
+                    className="font"
+                    variant="h5" 
+                    component="h5" >
+    
+                    Somos oque somos para um futuro melhor 
+                    
+                    </Typography>
+                        <Typography variant="h6"  component="h6" align="center" className='font'>Pokémon elétricos são um dos tipos mais fortes em Pokémon Go porque eles têm apenas uma fraqueza real (Pokémon terrestre). Seus ataques são potencializados pelo tempo chuvoso, então espere ver muitos outros treinadores usando-os se o tempo for previsível. </Typography>
+    
+                  
+
+                </Box>
+                <Box display="flex" justifyContent="center">
+                    <Box marginRight={1}>
+                    </Box>
+
+                </Box>
+            </Grid>
+
+            <Grid item xs={6} >
+                <Box p={1}>
+                    <img src="https://res.cloudinary.com/generation/image/upload/w_900,h_700,c_fill,q_auto:eco/brazil/2019/08/190603-GENERATION-469-1.jpg" alt="Eletric Pokemon" min-width='750px' height="450px" />
+                </Box>
+            </Grid>
+        </Grid>
+        </Box>
+       
         </>
-  );
+    );
+
 }
 
 export default Home;
